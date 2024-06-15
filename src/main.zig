f573a9742360a02e2dd34e54a8ffdfc42b7498f7
Jan Hendrik Farr <kernel@jfarr.cc>
2024-06-15 16:45:02 +0200

    no epoll, just timerfd
    Signed-off-by: Jan Hendrik Farr <kernel@jfarr.cc>

--- src/main.zig
+++ src/main.zig
@@ -15,11 +15,11 @@ pub fn main() !void {
 
     const timer_fd = try posix.timerfd_create(linux.CLOCK.REALTIME, linux.TFD{ ._0 = 0 });
 
-    const epoll_fd: i32 = @bitCast(@as(u32, (@truncate(linux.epoll_create1(0)))));
-    defer posix.close(epoll_fd);
-    if (epoll_fd < 0) {
-        return error.anyerror;
-    }
+    // const epoll_fd: i32 = @bitCast(@as(u32, (@truncate(linux.epoll_create1(0)))));
+    // defer posix.close(epoll_fd);
+    // if (epoll_fd < 0) {
+    //     return error.anyerror;
+    // }
 
     var now: posix.timespec = undefined;
     try posix.clock_gettime(linux.CLOCK.REALTIME, &now);
@@ -29,54 +29,58 @@ pub fn main() !void {
     try write_time_fmt(&now, w);
     try bw.flush();
 
+    // var tspec: linux.itimerspec = .{
+    //     .it_value = .{ .tv_sec = (@divTrunc(now.tv_sec, 60) + 1) * 60, .tv_nsec = 0 },
+    //     .it_interval = .{ .tv_sec = 60, .tv_nsec = 0 },
+    // };
     var tspec: linux.itimerspec = .{
         .it_value = .{ .tv_sec = now.tv_sec + 1, .tv_nsec = 0 },
         .it_interval = .{ .tv_sec = 1, .tv_nsec = 0 },
     };
     try posix.timerfd_settime(timer_fd, .{ .ABSTIME = true }, &tspec, null);
 
-    var timer_event: linux.epoll_event = .{
-        .events = linux.EPOLL.IN,
-        .data = linux.epoll_data{ .u32 = 0 },
-    };
+    // var timer_event: linux.epoll_event = .{
+    //     .events = linux.EPOLL.IN,
+    //     .data = linux.epoll_data{ .u32 = 0 },
+    // };
 
-    if (@as(i32, @bitCast(@as(u32, @truncate(linux.epoll_ctl(
-        epoll_fd,
-        linux.EPOLL.CTL_ADD,
-        timer_fd,
-        &timer_event,
-    ))))) < 0) {
-        return error.anyerror;
-    }
+    // if (@as(i32, @bitCast(@as(u32, @truncate(linux.epoll_ctl(
+    //     epoll_fd,
+    //     linux.EPOLL.CTL_ADD,
+    //     timer_fd,
+    //     &timer_event,
+    // ))))) < 0) {
+    //     return error.anyerror;
+    // }
 
-    var event_queue: [1]linux.epoll_event = undefined;
+    // var event_queue: [1]linux.epoll_event = undefined;
 
     while (true) {
-        const n_events: i32 = @bitCast(@as(u32, @truncate(linux.epoll_wait(
-            epoll_fd,
-            &event_queue,
-            event_queue.len,
-            -1,
-        ))));
-        if (n_events < 0) {
-            continue;
-        }
+        // const n_events: i32 = @bitCast(@as(u32, @truncate(linux.epoll_wait(
+        //     epoll_fd,
+        //     &event_queue,
+        //     event_queue.len,
+        //     -1,
+        // ))));
+        // if (n_events < 0) {
+        //     continue;
+        // }
 
-        for (event_queue[0..@as(u32, @bitCast(n_events))]) |_| {
-            var buf: [8]u8 = undefined;
-            // const t0 = t.read();
-            _ = try posix.read(timer_fd, &buf);
-            // const t1 = t.read();
+        // for (event_queue[0..@as(u32, @bitCast(n_events))]) |_| {
+        var buf: [8]u8 = undefined;
+        // const t0 = t.read();
+        _ = try posix.read(timer_fd, &buf);
+        // const t1 = t.read();
 
-            // std.debug.print("time to read: {} [ns]\n", .{t1 - t0});
+        // std.debug.print("time to read: {} [ns]\n", .{t1 - t0});
 
-            const dt: u64 = @bitCast(buf);
-            now.tv_sec += @intCast(dt);
+        const dt: u64 = @as(u64, @bitCast(buf)) * 1;
+        now.tv_sec += @intCast(dt);
 
-            try write_time_fmt(@ptrCast(&now), &w);
+        try write_time_fmt(@ptrCast(&now), &w);
 
-            try bw.flush();
-        }
+        try bw.flush();
+        // }
     }
 }
 
@@ -85,5 +89,6 @@ fn write_time_fmt(now: *posix.timespec, w: anytype) !void {
     const gm = c.localtime(@ptrCast(now));
     // const n = c.strftime(&buf, buf.len, "%a %b %d %H:%M:%S UTC %Y", gm);
     const n = c.strftime(&buf, buf.len, "%a %b %d %I:%M:%S %p %Z %Y", gm);
+    // const n = c.strftime(&buf, buf.len, "%a %b %d %I:%M %p %Z %Y", gm);
     try w.print("{s}\n", .{buf[0..n]});
 }
