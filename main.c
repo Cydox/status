889a99b84bf8dd8bc1ef867ae14865ac33f65676
Jan Hendrik Farr <kernel@jfarr.cc>
2024-11-28 15:04:02 +0100

    assertions
    Signed-off-by: Jan Hendrik Farr <kernel@jfarr.cc>

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
@@ -9,9 +10,12 @@
 #define BUF_SIZE 1024
 static char buf[BUF_SIZE];
 
-size_t write_time_fmt(const struct timespec *t, char *p, size_t p_len) {
+ssize_t write_time_fmt(const struct timespec *t, char *p, size_t p_len) {
 	struct tm *gm = localtime((const time_t *)t);
-	size_t n = strftime(p, p_len, "%a %b %d %I:%M:%S %p %Z %Y\n", gm);
+	assert(gm);
+
+	ssize_t n = strftime(p, p_len, "%a %b %d %I:%M:%S %p %Z %Y\n", gm);
+	assert(n > 0);
 
 	return n;
 }
@@ -20,8 +24,7 @@ int main (void) {
 
 	while (1) {
 		struct timespec now;
-		if (clock_gettime(CLOCK_REALTIME, &now) < 0)
-			exit(EXIT_FAILURE);
+		assert(!clock_gettime(CLOCK_REALTIME, &now));
 
 		now.tv_nsec = 0;
 
@@ -31,22 +34,20 @@ int main (void) {
 		};
 
 		int timer_fd = timerfd_create(CLOCK_REALTIME, 0);
-		if (timer_fd < 0)
-			exit(EXIT_FAILURE);
+		assert(timer_fd >= 0);
 
-		if (timerfd_settime(
+		assert(!timerfd_settime(
 			timer_fd,
 			TFD_TIMER_ABSTIME | TFD_TIMER_CANCEL_ON_SET,
 			&tspec,
 			0
-		) < 0)
-			exit(EXIT_FAILURE);
+		));
 
 		while (1) {
 			char read_buf[8];
 
-			size_t n = write_time_fmt(&now, buf, BUF_SIZE);
-			write(1, buf, n);
+			ssize_t n = write_time_fmt(&now, buf, BUF_SIZE);
+			assert(write(1, buf, n) == n);
 
 			n = read(timer_fd, read_buf, 8);
 			if (n != 8)
@@ -58,7 +59,6 @@ int main (void) {
 
 		close(timer_fd);
 	}
-
 
 	return EXIT_SUCCESS;
 }
