9029651ab8cf57358a7dd993d32b3b1ebe4959ba
Jan Hendrik Farr <kernel@jfarr.cc>
2024-11-29 11:15:42 +0100

    more assertions
    Signed-off-by: Jan Hendrik Farr <kernel@jfarr.cc>

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
@@ -21,10 +22,12 @@ ssize_t write_time_fmt(const struct timespec *t, char *p, size_t p_len) {
 }
 
 int main (void) {
+	int r;
 
 	while (1) {
 		struct timespec now;
-		assert(!clock_gettime(CLOCK_REALTIME, &now));
+		r = clock_gettime(CLOCK_REALTIME, &now);
+		assert(!r);
 
 		now.tv_nsec = 0;
 
@@ -36,28 +39,33 @@ int main (void) {
 		int timer_fd = timerfd_create(CLOCK_REALTIME, 0);
 		assert(timer_fd >= 0);
 
-		assert(!timerfd_settime(
+		timerfd_settime(
 			timer_fd,
 			TFD_TIMER_ABSTIME | TFD_TIMER_CANCEL_ON_SET,
 			&tspec,
 			0
-		));
+		);
+		assert(!r);
 
 		while (1) {
 			char read_buf[8];
 
 			ssize_t n = write_time_fmt(&now, buf, BUF_SIZE);
-			assert(write(1, buf, n) == n);
+			r = write(1, buf, n);
+			assert(r == n);
 
 			n = read(timer_fd, read_buf, 8);
-			if (n != 8)
+			if (n != 8) {
+				assert(errno == ECANCELED);
 				break;
+			}
 
 			uint64_t dt = *((uint64_t *)read_buf);
 			now.tv_sec += dt;
 		}
 
-		close(timer_fd);
+		r = close(timer_fd);
+		assert(!r);
 	}
 
 	return EXIT_SUCCESS;
